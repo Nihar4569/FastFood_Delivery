778c8bd30e39b8d097971417d42df9006f80b404
Nihar4569 <nihar4569@gmail.com>
2024-03-11 00:39:41 +0530

    INIT

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import Home from './screen/Home';
+import Login from './screen/Login';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
